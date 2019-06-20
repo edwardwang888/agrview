a23101971be3a78dad0a6cd87bb02659d5720566
Edward Wang <edwardwang888@gmail.com>
2019-06-20 00:59:48 +0000

    Implement GPS parsing

--- populate-db.py
+++ populate-db.py
@@ -6,24 +6,17 @@ from datetime import datetime
 import os
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Populate database with OO data.')
-    parser.add_argument('inputFile', help='input file containing OO data')
+    parser = argparse.ArgumentParser(description='Populate database with OO and GPS data.')
+    parser.add_argument('--oo', help='input file containing OO data')
+    parser.add_argument('--gps', help='input file containing GPS data')
     return parser.parse_args()
 
-def main():
-    args = parse_args()
-    subprocess.run(['python3',os.getenv('OO_PARSER'),args.inputFile,'.'])
+def populate_oo(args, connection):
+    subprocess.run(['python3',os.getenv('OO_PARSER'),args.oo,'.'])
     with open(os.getenv('OO_DIRECTORY') + 'timelist.txt','r') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
         filelist = next(csvreader)
         csvfile.close()
-
-    connection = pymysql.connect(host=os.getenv('MYSQL_INSTANCE'),
-                             user=os.getenv('MYSQL_USER'),
-                             password=os.getenv('MYSQL_PASSWORD'),
-                             db=os.getenv('DB_NAME'),
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
 
     # Eventually may want to remove filelist and look for files directly
     for filename in filelist:
@@ -39,6 +32,33 @@ def main():
                 sql = "INSERT INTO `" + os.getenv('OO_TABLE') + "` (`SysTime`,`" + \
                       os.getenv('OO_KEY2') + "`,`" + os.getenv('OO_KEY3') + "`) VALUES(%s, %s, %s)"
                 cursor.execute(sql, (systime, oo_key2_data, oo_key3_data))
+
+def populate_gps(args, connection):
+    subprocess.run(['python3','gps_to_csv.py',args.gps])
+    with open(args.gps + '.csv','r') as csvfile:
+        csvreader = csv.reader(csvfile,delimiter=',')
+        for row in csvreader:
+            latitude = ('+' if row[3] == 'N' else '-') + row[2]
+            longtitude = ('+' if row[5] == 'E' else '-') + row[4]
+            print(latitude + ' ' + longtitude)
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO `GPS` (`SysTime`,`GPSTime`,`Latitude`,`Longtitude`,`Altitude`,`GroundElevation`) VALUES(%s, %s, %s, %s, %s, %s)"
+                cursor.execute(sql, (row[0], row[1], latitude, longtitude, row[6], 0))
+
+def main():
+    args = parse_args()
+    connection = pymysql.connect(host=os.getenv('MYSQL_INSTANCE'),
+                             user=os.getenv('MYSQL_USER'),
+                             password=os.getenv('MYSQL_PASSWORD'),
+                             db=os.getenv('DB_NAME'),
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+    if args.oo != None:
+        populate_oo(args, connection)
+
+    if args.gps != None:
+        populate_gps(args, connection)
 
     # Close connection to database
     connection.commit()
