431f174236ce0acabb09eecfdfbc793743105755
Edward Wang <edwardwang888@gmail.com>
2019-07-01 03:59:43 +0000

    Check for latest upload datetime instead

--- check-upload-finished.py
+++ check-upload-finished.py
@@ -1,29 +1,39 @@
 import json
 import boto3
 import os
+from datetime import datetime, timezone
+
+def get_latest_date(response):
+    latest_date = None
+    for entry in response['Contents']:
+        if latest_date == None or entry['LastModified'] > latest_date:
+            latest_date = entry['LastModified']
+
+    return latest_date
 
 def lambda_handler(event, context):
     print(event)
 
-    # Check if event has previous execution results
-    try:
-        numfiles = event['Input']['taskresult']['Payload']['Count']
-        retries = event['Input']['taskresult']['Payload']['Retries']
-    except KeyError:
-        return {"Equal": False, "Count": 0, "Retries": 0}
-
     client = boto3.client('s3',
                       aws_access_key_id=os.getenv('MY_ACCESS_KEY_ID'),
                       aws_secret_access_key=os.getenv('MY_SECRET_ACCESS_KEY'))
-
-    # Count number of files for this particular prefix
     mybucket = os.getenv('MY_BUCKET')
-    count = 0
     response = client.list_objects_v2(Bucket=mybucket, Prefix=event['Input']['Key'])
-    count += response['KeyCount']
+    latest_date = get_latest_date(response)
     while response['IsTruncated']:
         response = client.list_objects_v2(Bucket=mybucket, Prefix=event['Input']['Key'],
                                        ContinuationToken=response['NextContinuationToken'])
-        count += response['KeyCount']
+        new_date = get_latest_date(response)
+        if new_date > latest_date:
+            latest_date = new_date
 
-    return {"Equal": count == numfiles, "Count": count, "Retries": retries + 1}
+    print('Latest datetime is: {}'.format(latest_date))
+    print('Current datetime is: {}'.format(datetime.now(timezone.utc)))
+    if (datetime.now(timezone.utc) - latest_date).total_seconds() > 18000:
+        return {"Done": True, "Retries": 0}
+    else:
+        try:
+            retries = event['Input']['taskresult']['Payload']['Retries']
+        except KeyError:
+            retries = 1
+        return {"Done": False, "Retries": retries + 1}
