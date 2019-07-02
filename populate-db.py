5c3a5effb9fd046f56be511c8c9200488692430e
Edward Wang <edwardwang888@gmail.com>
2019-07-02 19:59:35 +0000

    Implement data validation

--- populate-db.py
+++ populate-db.py
@@ -8,6 +8,7 @@ import botocore.config
 import urllib.parse
 import json
 import os
+import math
 
 print('Loading function')
 client = boto3.client('s3',
@@ -31,7 +32,10 @@ def connectDB():
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
-def populate_oo(connection, oo_file):
+def populate_oo(connection, oo_file, numseconds):
+    """
+    Parses OO data and checks if the number of readings is consistent with the number of seconds of flight.
+    """
     print('oo_file: {}'.format(oo_file))
     s3.meta.client.download_file(os.getenv('MY_BUCKET1'), oo_file, '/tmp/oo_file.txt')
     subprocess.run(['python3',os.getenv('OO_PARSER'),'/tmp/oo_file.txt','/tmp'])
@@ -40,6 +44,10 @@ def populate_oo(connection, oo_file):
         csvreader = csv.reader(csvfile, delimiter=',')
         filelist = next(csvreader)
         csvfile.close()
+
+    if math.fabs(numseconds - len(filelist)) > 1000:
+        print('OO readings: {}'.format(len(filelist)))
+        raise Exception('Number of OO readings inconsistent with duration of flight')
 
     # Eventually may want to remove filelist and look for files directly
     for filename in filelist:
@@ -66,12 +74,37 @@ def populate_gps(connection, gps_file):
     with open(dest + '.csv','r') as csvfile:
         csvreader = csv.reader(csvfile,delimiter=',')
         next(csvreader)
+        mindate = None
+        maxdate = None
+        count = 0
         for row in csvreader:
+            count += 1
             latitude = ('+' if row[3] == 'N' else '-') + row[2]
             longtitude = ('+' if row[5] == 'E' else '-') + row[4]
+            systime = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f')
+            if mindate == None or systime < mindate:
+                mindate = systime
+            if maxdate == None or systime > maxdate:
+                maxdate = systime
             with connection.cursor() as cursor:
                 sql = "INSERT INTO `GPS` (`SysTime`,`GPSTime`,`Latitude`,`Longtitude`,`Altitude`,`GroundElevation`) VALUES(%s, %s, %s, %s, %s, %s)"
                 cursor.execute(sql, (row[0], row[1], latitude, longtitude, row[6], row[7]))
+
+        print('Min date is:')
+        print(mindate)
+        print('Max date is:')
+        print(maxdate)
+        print('Total elapsed time is:')
+        difference = maxdate - mindate
+        print(difference)
+        numseconds = difference.total_seconds()
+        print('Seconds elapsed is:')
+        print(numseconds)
+        print('Number of GPS readings:')
+        print(count)
+        if math.fabs(numseconds - count) > 1000:
+            raise Exception('Number of GPS readings inconsistent with flight duration')
+        return numseconds
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
@@ -101,6 +134,18 @@ def lambda_handler(event, context):
 
         print('oo file: {}'.format(oo_file))
         print('gps file: {}'.format(gps_file))
+        numseconds = populate_gps(connection, gps_file)
+        populate_oo(connection, oo_file, numseconds)
+
+        # Check HCount and ICount
+        hcount = event['Input']['taskresult']['Payload']['HCount']
+        icount = event['Input']['taskresult']['Payload']['ICount']
+        print('HCount: {}'.format(hcount))
+        print('ICount: {}'.format(icount))
+        if math.fabs(hcount - numseconds) > 1000:
+            raise Exception('Number of H Images inconsistent with flight duration')
+        if math.fabs(icount - 10 * numseconds) > 10000:
+            raise Exception('Number of I Images inconsistent with flight duration')
 
         # Populate FlightLookup table
         data = key.split('/')
@@ -114,8 +159,6 @@ def lambda_handler(event, context):
                                      data[0], data[1]))
                 print('Finished populating FlightLookup')
 
-        populate_oo(connection, oo_file)
-        populate_gps(connection, gps_file)
         connection.commit()
         connection.close()
 
